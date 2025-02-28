565dced332cc8f97539615b886739511e4455baf
kritz999 <kritikabdas007@gmail.com>
2025-02-28 15:07:19 +0530

    [kritika] add: UC10_NameFieldValidation

--- EmployeePayRoll/EmployeePayRoll/src/main/java/com/example/EmployeePayRoll/EmployeeDTO.java
+++ EmployeePayRoll/EmployeePayRoll/src/main/java/com/example/EmployeePayRoll/EmployeeDTO.java
@@ -1,9 +1,16 @@
 package com.example.EmployeePayRoll;
 import lombok.Data;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.springframework.validation.annotation.Validated;
+
 // Data Transfer Object (DTO) for Employee.
 @Data
 public class EmployeeDTO {
+    @NotBlank(message = "Name cannot be empty.")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "First letter in uppercase, minimum 3 letters are required.")
     private String name;
     private double salary;
 }
